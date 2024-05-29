2201b68f5577a58e45e52e5e7907658ebab986db
Alejandro Campos <134987912+alejocampos1@users.noreply.github.com>
2024-05-29 14:21:18 -0600

    Proyecto 2 - Carga de datos

---
+++ Proyecto 2/Proyecto2.py
@@ -0,0 +1,12 @@
+# Importar librerias
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Cargar datos
+
+sales_df = pd.read_csv('sales_data.csv')
+
